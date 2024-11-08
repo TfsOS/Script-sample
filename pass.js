764797c98312b8669e66b005ab0d52ac781ae4cf
TfsOS <acr.tfs@gmail.com>
2024-11-08 21:00:58 +0530

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,14 @@
+// pass.js
+const users = [
+    { email: "RG@TFS.ORG", password: "RIYANGHOSH", username: "RiyanOfficial" },
+    { email: "TfsOs@tfs.org", password: "TfsOs", username: "TfsOs" },
+    { email: "Md@tfs.org", password: "MDMD", username: "MAINAK" },
+    { email: "AB@tfs.org", password: "ABAB", username: "ANGSHUMAN" },
+    { email: "Ankush@TFS.ORG", password: "ADAD", username: "ANKUSH" },
+    { email: "Swarnadeep@TFS.ORG", password: "SGSG", username: "SWARNADEEP" },
+    { email: "Trishan@TFS.ORG", password: "TBTB", username: "TRISHAN" },
+    { email: "Ridhan@TFS.ORG", password: "RIRI", username: "RIDHAN" },
+    { email: "Abhirup@TFS.ORG", password: "ABAB", username: "ABHIRUP" }
+];
+
+
